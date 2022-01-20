d7029a4e6ba530991c3da85fd945d246c373f266
Bartosz-Wegrzyn <bartosz.wegrzyn@icloud.com>
2022-01-20 12:34:38 +0100

    special, numbers added to create_password def

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import random
 
-abc = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890!@#$%^&*()"
-
-
-def create_passwor(dlugosc):
+def create_passwor(dlugosc, special=True, numbers=True):
     out = ""
+    abc = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM"
+    if special == True:
+        abc += '!@#$%^&*()'
+    if numbers == True:
+        abc += '1234567890'
+
     for x in range(dlugosc):
-        pozycja = random.randint(0, 71)
-        #  dasdasdaprint(abc[pozycja])pozycja
+        pozycja = random.randint(0, len(abc)-1)
         out += (abc[pozycja])
-        print(out)
+    return out
+
 while True:
     a = input('podaj dlugosc hasla:')
     create_passwor(int(a))
